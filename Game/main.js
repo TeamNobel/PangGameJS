d49b7f83303fb1ad4853c945d3ed6e9c9889eefb
IvanMladenov <ismladenov@gmail.com>
2016-01-26 14:42:31 +0200

    merge with test folder, make the dwarf to move only on canvas

--- Game/main.js
+++ Game/main.js
@@ -7,55 +7,18 @@ background.onload = function () {
     ctx.drawImage(background, 0, 0);
 };
 
-var rectangles = [];
-
+//var rectangles = [];
 var balls = [
     new ball(10, 250, 6, 'blue'),
     new ball(100, 150, 15, 'red'),
     new ball(100, 100, 30, 'pink'),
-    new ball(100, 50, 45, 'gold')
+    //new ball(100, 50, 45, 'gold')
+],
+    player = player = new Player(canvas.width / 2, canvas.height - 33);
 
-];
+var input = new Input();
+attachListeners(input);
 
-function rectCircleColliding(circle, rect) {
-    var distX = Math.abs(circle.x - rect.x - rect.width / 2);
-    var distY = Math.abs(circle.y - rect.y - rect.height / 2);
-
-    if (distX > (rect.width / 2 + circle.radius)) {
-        return false;
-    }
-    if (distY > (rect.height / 2 + circle.radius)) {
-        return false;
-    }
-
-    if (distX <= (rect.width / 2)) {
-        return true;
-    }
-    if (distY <= (rect.height / 2)) {
-        return true;
-    }
-
-    var dx = distX - rect.width / 2;
-    var dy = distY - rect.height / 2;
-    return (dx * dx + dy * dy <= (circle.radius * circle.radius));
-}
-
-function changeDirection(ball, rect) {
-    if (ball.y + ball.radius === rect.y || ball.y - ball.radius === rect.y + rect.height) {
-        ball.vy *= -1;
-    } else if (ball.x + ball.radius === rect.x || ball.x - ball.radius === rect.x + rect.width) {
-        ball.vx *= -1;
-    } else {
-        if (ball.y >= rect.y && ball.y <= rect.y + rect.height) {
-            ball.vx *= -1;
-        } else if (ball.x >= rect.x && ball.x <= rect.x + rect.width) {
-            ball.vy *= -1;
-        } else {
-            ball.vy *= -1;
-            ball.vx *= -1;
-        }
-    }
-}
 
 function checkForCanvasColide(ball) {
     if (ball.y > canvas.height - ball.radius || ball.y < ball.radius) {
@@ -72,18 +35,70 @@ function draw() {
         checkForCanvasColide(ball);
         ball.draw();
 
-//            if (rectCircleColliding(ball, rect)) {
-//                changeDirection(ball, rect);
-//            }
         ball.x += ball.vx;
         ball.y += ball.vy;
-        ball.vy+=0.1;
-        if (ball.maxHeight>ball.y){
-            ball.y=ball.maxHeight;
+        ball.vy += 0.1;
+        if (ball.maxHeight > ball.y) {
+            ball.y = ball.maxHeight;
         }
 
     });
 
-
+    tick();
+    player.render(ctx);
     raf = window.requestAnimationFrame(draw);
 }
+
+function tick() {
+    player.movement.right = !!input.right;
+    player.movement.left = !!input.left;
+
+    player.updateAnimationSettings();
+}
+
+function update() {
+    this.tick();
+    this.render(ctx);
+    requestAnimationFrame(update);
+}
+
+    //
+//function rectCircleColliding(circle, rect) {
+//    var distX = Math.abs(circle.x - rect.x - rect.width / 2);
+//    var distY = Math.abs(circle.y - rect.y - rect.height / 2);
+//
+//    if (distX > (rect.width / 2 + circle.radius)) {
+//        return false;
+//    }
+//    if (distY > (rect.height / 2 + circle.radius)) {
+//        return false;
+//    }
+//
+//    if (distX <= (rect.width / 2)) {
+//        return true;
+//    }
+//    if (distY <= (rect.height / 2)) {
+//        return true;
+//    }
+//
+//    var dx = distX - rect.width / 2;
+//    var dy = distY - rect.height / 2;
+//    return (dx * dx + dy * dy <= (circle.radius * circle.radius));
+//}
+
+//function changeDirection(ball, rect) {
+//    if (ball.y + ball.radius === rect.y || ball.y - ball.radius === rect.y + rect.height) {
+//        ball.vy *= -1;
+//    } else if (ball.x + ball.radius === rect.x || ball.x - ball.radius === rect.x + rect.width) {
+//        ball.vx *= -1;
+//    } else {
+//        if (ball.y >= rect.y && ball.y <= rect.y + rect.height) {
+//            ball.vx *= -1;
+//        } else if (ball.x >= rect.x && ball.x <= rect.x + rect.width) {
+//            ball.vy *= -1;
+//        } else {
+//            ball.vy *= -1;
+//            ball.vx *= -1;
+//        }
+//    }
+//}
---
+++ Game/player.js
@@ -0,0 +1,61 @@
+var Player = new (function () {
+    function Player(x, y) {
+        this.position = new Vector2(x, y);
+        this.velocity = 3;
+        this.width = 32;
+        this.height = 33;
+        this.row = 0;
+        this.col = 0;
+        this.numberOfFrames = 5;
+        this.movement = {left: false, right: false};
+        this.isSetLeftAnimation = false;
+        this.isSetRightAnimation = false;
+        this.animation = new Animation(this.width, this.height, this.row, this.col, this.numberOfFrames, 'sprites.png', 10, 23, 1);
+        this.boundingBox = new Rectangle(x, y, this.width, this.height);
+    }
+
+    Player.prototype.update = function () {
+        if (this.movement.left) {
+            if ((this.position.x - this.velocity) > 0) {
+                this.position.x -= this.velocity;
+            }
+        } else if (this.movement.right) {
+            if (this.position.x + this.velocity < canvas.width - this.width) {
+                this.position.x += this.velocity;
+            }
+        }
+
+        this.animation.position.set(this.position.x, this.position.y);
+        //this.boundingBox.position.x = this.position.x;
+        //this.boundingBox.position.y = this.position.y;
+
+        this.animation.update();
+    };
+
+    Player.prototype.render = function (ctx) {
+        this.animation.draw(ctx);
+    };
+
+    Player.prototype.updateAnimationSettings = function () {
+        if (this.movement.left && !this.isSetLeftAnimation) {
+            this.isSetLeftAnimation = true;
+            this.isSetRightAnimation = false;
+            this.animation.setColumn(11);
+            this.animation.setLimit(5);
+        } else if (this.movement.right && !this.isSetRightAnimation) {
+            this.isSetRightAnimation = true;
+            this.isSetLeftAnimation = false;
+            this.animation.setColumn(0);
+            this.animation.setLimit(5);
+        } else if (!this.movement.right && !this.movement.left) {
+            this.isSetLeftAnimation = false;
+            this.isSetRightAnimation = false;
+            this.animation.setColumn(5);
+            this.animation.setLimit(0);
+        }
+
+        this.update();
+    };
+
+    return Player;
+});
